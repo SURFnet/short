bb21a96cba7917bcb7786e0bdf11ee9e7853177c
Sergio Gómez <sergio@uco.es>
2020-08-27 09:28:53 +0200

    Check admin role with SAML attributes

--- src/Security/User.php
+++ src/Security/User.php
@@ -7,10 +7,12 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User implements UserInterface
 {
     private $userid;
+    private $roles = [];
 
-    public function __construct(string $username)
+    public function __construct(string $username, bool $admin = false)
     {
         $this->userid = $username;
+        $this->roles[] = $admin ? 'ROLE_ADMIN' : 'ROLE_USER';
     }
 
     /**
@@ -28,20 +30,14 @@ class User implements UserInterface
      */
     public function getRoles(): array
     {
-        // FIXME: need much better way to authorize admins.
-        // or at the very least move this list to config
-        if(in_array($this->userid, [
-            'd55f67a470505f56d02b1be3a6321ea72fd3f539',
-            '0d9f01ee321f2a480b6a296389887b66bfb65db2',
-            'a8fb1916b4116b0e42ee1b991d37ffd66828b30c',
-            '59cd042e0c992b73ae08e22b7fcae2b595adeaa1',
-            'dd8660e3b354b811b87ef3d7e7cdb232dc8fd325',
-	    '7235307d8f46a972d7c747a5584ce9662fa923a5',
-            ] )) {
-            return ['ROLE_USER', 'ROLE_ADMIN'];
-	}
+        $roles = $this->roles;
 
-        return ['ROLE_USER'];
+        // guarantees that a user always has at least one role for security
+        if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return array_unique($roles);
     }
 
     /**
