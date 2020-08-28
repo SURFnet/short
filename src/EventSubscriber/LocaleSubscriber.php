d8973f511911bedbfd7e5c1c91b5fabd34584b32
Sergio Gómez <sergio@uco.es>
2020-08-28 09:08:44 +0200

    Add locale listener to set client accept language

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct(string $defaultLocale)
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        if ('undefined' == $request->getLocale()) {
+            if ($locale = $request->getSession()->get('_locale')) {
+                $request->setLocale($locale);
+            } else {
+                $request->setLocale($request->getPreferredLanguage());
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
