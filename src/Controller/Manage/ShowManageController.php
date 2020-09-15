ad269ab779684132ffbe7ba32e7447c819cd2f6e
Sergio Gómez <sergio@uco.es>
2020-09-15 09:07:41 +0200

    Add show controller
    Creation was implemented in index controllers. 
    User is redirected to show controller after creation

---
+++ src/Controller/Manage/ShowManageController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Controller\Manage;
+
+
+use App\Entity\ShortUrl;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @Route("/manage/{shortUrl}/show", name="app_manage_show", methods={"GET"})
+ * @IsGranted("SHOW", subject="instance")
+ */
+final class ShowManageController extends AbstractController
+{
+    public function __invoke(ShortUrl $instance): Response
+    {
+        if ($instance->getDeleted()) {
+            throw $this->createNotFoundException();
+        }
+
+        /** @var UserInterface $user */
+        $user = $this->getUser();
+
+        if ($user->getUsername() !== $instance->getOwner()) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('manage/show.html.twig', [
+            'short_url' => $instance
+        ]);
+    }
+}
