af44554569c7498515bf0e411327ff662d03c07f
Sergio Gómez <sergio@uco.es>
2020-09-15 09:02:31 +0200

    Add ShortUrl voter to check user permissions
    Only owners can edit, show or delete.

---
+++ src/Security/Voter/ShortUrlVoter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\ShortUrl;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ShortUrlVoter extends Voter
+{
+    public const DELETE = 'DELETE';
+    public const EDIT = 'EDIT';
+    public const SHOW = 'SHOW';
+
+    protected function supports($attribute, $subject)
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE, self::SHOW])
+            && $subject instanceof ShortUrl;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        /** @var $subject ShortUrl */
+
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        return $subject->getOwner() === $user->getUsername();
+    }
+}
