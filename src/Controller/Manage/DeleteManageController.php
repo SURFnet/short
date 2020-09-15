118d8c8c5d8ee200e793bf6fc6d6c97eaa32c941
Sergio Gómez <sergio@uco.es>
2020-09-15 09:08:42 +0200

    Add controllers to edit and delete ShortUrls

---
+++ src/Controller/Manage/DeleteManageController.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Controller\Manage;
+
+
+use App\Entity\ShortUrl;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/manage/{shortUrl}/delete", name="app_manage_delete", methods={"POST"})
+ * @Security("is_granted('DELETE', instance) or is_granted('ROLE_ADMIN')")
+ */
+final class DeleteManageController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function __invoke(Request $request, ShortUrl $instance)
+    {
+        if ($instance->getDeleted()) {
+            throw $this->createNotFoundException();
+        }
+
+        if (!$this->isCsrfTokenValid('delete', $request->request->get('token'))) {
+            return $this->redirectToRoute('app_manage_index');
+        }
+
+        $instance->setDeleted(true);
+        $instance->setUpdated();
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'short_url.deleted_successfully');
+
+        return $this->redirectToRoute('app_manage_index');
+    }
+}
