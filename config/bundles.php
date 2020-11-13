9c7e7312e308c5b6dbb65452fbe177e52c08e302
Sergio Gómez <sergio@uco.es>
2020-11-13 09:46:32 +0100

    Install webpack encore bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
