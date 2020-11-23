f212d1c962a3831b0891ae8b0e212126861d9d3c
Sergio Gómez <sergio@uco.es>
2020-11-23 08:52:54 +0100

    Set preferred language from available locales

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -5,31 +5,44 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
-    private $defaultLocale;
+    private $locales;
 
-    public function __construct(string $defaultLocale)
+    public function __construct(array $locales, string $defaultLocale)
     {
-        $this->defaultLocale = $defaultLocale;
+        $this->locales = array_keys($locales);
+
+        if (!in_array($defaultLocale, $this->locales, true)) {
+            $validLocales = implode(", ", $this->locales);
+
+            throw new \UnexpectedValueException(sprintf('The default locale ("%s") must be one of "%s".', $defaultLocale, $validLocales));
+        }
+
+        // Add the default locale at the first position of the array,
+        // because Symfony\HttpFoundation\Request::getPreferredLanguage
+        // returns the first element when no an appropriate language is found
+        array_unshift($this->locales, $defaultLocale);
+        $this->locales = array_unique($this->locales);
     }
 
     public function onKernelRequest(RequestEvent $event)
     {
-        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+        $request = $event->getRequest();
+
+        if (!$event->isMasterRequest()) {
             return;
         }
 
-        $request = $event->getRequest();
 
         if ('undefined' === $request->getLocale()) {
             if ($locale = $request->getSession()->get('_locale')) {
                 $request->setLocale($locale);
-            } elseif ($locale = $request->getPreferredLanguage()) {
-                $request->setLocale($locale);
+            } else {
+                $preferredLanguage = $request->getPreferredLanguage($this->locales);
+                $request->setLocale($preferredLanguage);
             }
         }
     }
