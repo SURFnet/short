5054540582569a6e52d0e606cc805c1c0b152ba9
Sergio Gómez <sergio@uco.es>
2020-12-11 09:20:27 +0100

    Delete ShortUrl using messages

---
+++ src/MessageHandler/ShortUrl/DeleteShortUrlMessageHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\MessageHandler\ShortUrl;
+
+use App\Entity\ShortUrl;
+use App\Message\ShortUrl\DeleteShortUrlMessage;
+use App\Repository\ShortUrlRepository;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+final class DeleteShortUrlMessageHandler implements MessageHandlerInterface
+{
+    /**
+     * @var ShortUrlRepository
+     */
+    private $shortUrlRepository;
+
+    public function __construct(ShortUrlRepository $shortUrlRepository)
+    {
+        $this->shortUrlRepository = $shortUrlRepository;
+    }
+
+    public function __invoke(DeleteShortUrlMessage $message)
+    {
+        $id = $message->getId();
+
+        $shortUrl = $this->shortUrlRepository->find($id);
+
+        if (!$shortUrl instanceof ShortUrl || $shortUrl->getDeleted()) {
+            throw new \RuntimeException('ShortUrl does not exist');
+        }
+
+        $shortUrl->setDeleted(true);
+        $shortUrl->setUpdated();
+    }
+}
