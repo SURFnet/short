390de377ef35bafd617bf72b8bcf5515e17a399f
Sergio Gómez <sergio@uco.es>
2021-02-08 08:46:40 +0100

    Change input long url from text type to textarea

--- src/Form/ShortUrlType.php
+++ src/Form/ShortUrlType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Form\Model\ShortUrlModel;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,7 @@ final class ShortUrlType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('longUrl', UrlType::class, [
-            'default_protocol' => null,
+        $builder->add('longUrl', TextareaType::class, [
             'attr' => [
                 'class' => 'form-control',
                 'autocomplete' => 'off',
