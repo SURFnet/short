5d82543ec894b561a154e66f6df40f936a3e5262
Sergio Gómez <sergio@uco.es>
2021-02-17 14:06:17 +0100

    Add native oidc dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
